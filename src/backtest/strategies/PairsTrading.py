69feb8645800f7ec5cac7a971fe4622ab682aa98
Louis Spencer <louisspencer87@berkeley.edu>
2023-03-22 03:31:06 -0700

    updated jupyter notebook

--- src/backtest/strategies/PairsTrading.py
+++ src/backtest/strategies/PairsTrading.py
@@ -1,32 +1,43 @@
-import pandas as pd
-
-from statsmodels.regression.rolling import RollingOLS
 from backtesting import Strategy
 from backtesting.lib import crossover, cross
+from statsmodels.tools import add_constant
+from statsmodels.regression.rolling import RollingOLS
 
+import pandas as pd
+import numpy as np
 
 class PairsTrading(Strategy):
     ########### INDICATOR PARAMETERS ##################
-    z_window = 24
-    hedge_ratio_window = 24
+    z_window = 72
+    hedge_ratio_window = 72
     
-    z_thresh_upper = 1
-    z_thresh_lower = -1
+    z_thresh_upper = 3
+    z_thresh_lower = -2
     
-    X = 'QNT_ETH_GATEIO'
+    X = 'ETH_USD_COINBASE'
     Y = 'AAVE_ETH_BINANCE'
     ###################################################
-    
-    def rolling_hedge_ratios(X, Y, window):
-        return RollingOLS(endog = Y, exog = X, window = window).fit().params
-    
-    def rolling_spread(X, Y, rolling_hedge_ratios):
-        return Y - rolling_hedge_ratios * X
-    
+        
     def z(data, window):
-        data = pd.Series(data.flatten())
+        data = pd.Series(data)
         return (data - data.rolling(window = window).mean()) / data.rolling(window = window).std()
     
+    def h(self, window):
+        
+        X = pd.DataFrame(add_constant(self.data[PairsTrading.X]))
+        X = X.rename({0:'intercept', 1:PairsTrading.X}, axis = 1)
+        Y = pd.DataFrame(self.data[PairsTrading.Y])
+        
+        rolling_hedge_ratio = RollingOLS(
+            endog = Y, 
+            exog = X, 
+            window = self.hedge_ratio_window).fit().params[PairsTrading.X]
+        
+        return rolling_hedge_ratio
+    
+    def s(self, h):
+        return self.data[PairsTrading.Y] - h * self.data[PairsTrading.X]
+
     def name():
         return 'PairsTrading'
 
@@ -39,40 +50,36 @@ class PairsTrading(Strategy):
         super().init()
         
         # Define indicator value arrays
-        self.hedge_ratios = self.I(
-            PairsTrading.rolling_hedge_ratios,
-            pd.DataFrame(self.data[PairsTrading.X]),
-            pd.DataFrame(self.data[PairsTrading.Y]),
+        
+        self.rolling_hedge_ratio = self.I(
+            self.h,
             self.hedge_ratio_window
         )
         
         self.spread = self.I(
-            PairsTrading.rolling_spread,
-            self.data[PairsTrading.X],
-            self.data[PairsTrading.Y],
-            self.hedge_ratios
+            self.s,
+            self.rolling_hedge_ratio.flatten()
         )
-        
+
         self.rolling_spread_z_score = self.I(
             PairsTrading.z,
-            self.spread,
+            self.spread.flatten(),
             self.z_window
         )
-
+        
     # Define how the backtest acts at each step of iterating through the dataset
-    def next(self):        
-        entry_signal = (
-            not crossover(self.rolling_spread_z_score, self.z_thresh_lower) and 
-            cross(self.rolling_spread_z_score, self.z_thresh_lower)
-        )
-        exit_signal = crossover(self.rolling_spread_z_score, self.z_thresh_upper)
-
+    def next(self): 
+        if self.rolling_hedge_ratio[-1] < 0:
+            return
+        
+        
+        entry_signal = (self.rolling_spread_z_score[-2] > self.z_thresh_lower) and (self.rolling_spread_z_score[-1] < self.z_thresh_lower)
+        exit_signal = (self.rolling_spread_z_score[-2] < self.z_thresh_upper) and (self.rolling_spread_z_score[-1] > self.z_thresh_upper)
+                
         # Close position
         if (self.position and exit_signal):
             self.position.close()
 
-        # Open position--buy asset (this does not do shorting yet)
+        # Open position
         elif (not self.position) and entry_signal:
-            # Trailing stop adjustment
-            self.buy(size = 0.1, sl = self.data.Close[-1] * 0.9)
-            
+            self.buy(size = 0.5, sl = self.data.Close[-1] * 0.9)
