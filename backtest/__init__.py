f2e402b6752d2d39b0e6b7bd33bd1ac25cce84ed
Louis <louisspencer87@gmail.com>
2023-12-25 14:12:50 -0800

    added imports to __init__.py for the backtest package

---
+++ backtest/__init__.py
@@ -0,0 +1,7 @@
+from .core.wfo.walk_forward_optimization import WalkForwardOptimization
+from .core.simulation.pbo import pbo
+from .core.performance.performance_metrics import *
+from .core.performance.performance_metrics_pbo import sortino
+from .strategies.ma_crossover import MACrossOver
+from .strategies.bollinger_bands import BollingerBands
+from .strategies.linear_regression import LogisticRegressionStrategy
