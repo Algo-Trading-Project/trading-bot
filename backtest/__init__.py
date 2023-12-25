fd44b6747da8d9323e1a8714e658f533c6fc115c
Louis <louisspencer87@gmail.com>
2023-12-25 14:57:43 -0800

    Import core and strategies modules

--- backtest/__init__.py
+++ backtest/__init__.py
@@ -1,9 +1,2 @@
-from .core.wfo.walk_forward_optimization import WalkForwardOptimization
-from .core.simulation.pbo import pbo
-from .core.performance.performance_metrics import *
-from .core.performance.performance_metrics_pbo import sortino
-from .strategies.ma_crossover import MACrossOver
-from .strategies.bollinger_bands import BollingerBands
-from .strategies.linear_regression import LogisticRegressionStrategy
-from . import backtester
-from .backtester import Backtester
+import .core
+import .strategies
